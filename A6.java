1a1c9c311d5eaf406ecc6c4c72cae867bc075a39
Faust060 <gamaevmagomed060@gmail.com>
2024-11-04 19:18:08 +0300

    Delete A6.java

--- A6.java
+++ A6.java
@@ -2,7 +2,7 @@ public class A6 {
     public static void main(String[] args) {
         Phone phone1 = new Phone();
         phone1.setModel("X5");
-        phone1.setNumber(890055535);
+        phone1.setNumber("890055535");
         phone1.setWeight(2500);
         phone1.setReciveCall("Мишка");
         phone1.specificationsPhone();
@@ -12,30 +12,31 @@ public class A6 {
 }
 
 
-class Phone{
-    private int number;
-    private int weight;
+class Phone {
+    private String number;
+    private double weight;
     private String name;
     private String model;
-    void setModel (String modelPhone){
+
+    void setModel(String modelPhone) {
         model = modelPhone;
     }
-    void setReciveCall(String nameCall){
+    void setReciveCall(String nameCall) {
         name = nameCall;
     }
-    void  reciveCall(){
-        System.out.println("Вам звонит: "+name);
+    void reciveCall() {
+        System.out.println("Вам звонит: " + name);
     }
-    public void setNumber(int numberUser){
+    public void setNumber(String numberUser) {
         number = numberUser;
     }
-    public int getNumber(){
+    public String getNumber() {
         return number;
     }
-    public void setWeight(int weightPhone){
+    public void setWeight(int weightPhone) {
         weight = weightPhone;
     }
-    void specificationsPhone(){
+    void specificationsPhone() {
         System.out.println("Модель телефона: " + model + ", вес телефона: " + weight);
     }
 }
